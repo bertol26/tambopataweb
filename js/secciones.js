04ceee0564169daeafd72c3cb471eb186eca47a0
bertol26 <bertolfranco26@gmail.com>
2022-02-23 22:23:47 -0500

    correcciones

--- js/secciones.js
+++ js/secciones.js
@@ -4,3 +4,12 @@ $(document).ready(function () {
 		$('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 1000);
 	});
 });
+
+
+
+const navtoggle = document.querySelector('.icon-mobile');
+const navmenumobile = document.querySelector('.nav-list');
+
+navtoggle.addEventListener('click', () => {
+  navmenumobile.classList.toggle('nav-visible');
+})
